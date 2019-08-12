c23fe7642907d869c65f5a9c8d751778a5101f34
caihongquan <caihongquan@medlinker.com>
2019-08-12 12:41:09 +0800

    feat: 添加eslint验证工具及自动格式化代码

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
+  ],
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  env: {
+    "browser": true,
+    "jest": true
+  },
+  rules: {
+    "prettier/prettier": ["error", { "singleQuote": true }]
+  },
+  settings: {
+    "react": {
+      "pragma": "React",
+      "version": "detect"
+    }
+  },
+  parser: "@typescript-eslint/parser"
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-const App: React.FC = () => {
+const App = (): JSX.Element => {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,7 +20,7 @@ const App: React.FC = () => {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
